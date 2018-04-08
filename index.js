1686c3947146486b7e01566b390045b26f0323f5
Michael Clarke <bspride11@gmail.com>
2018-04-07 19:25:31 -0700

    Access table name from env variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 console.log('Loading function');
+var table = process.env.TABLE_NAME;
 
 exports.handler = (event, context, callback) => {
     const message = event.Records[0].Sns.Message;
@@ -9,7 +10,7 @@ exports.handler = (event, context, callback) => {
 
 function GameSim(home, away) {
     // fetch team info from dynamo db
-    
+
     // run simulation
     // update dynamo with results
 }
